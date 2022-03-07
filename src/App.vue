698fec7ec08fb32c4a1c0d1f7c01be5781ce4414
june0nara <honeybee@snu.ac.kr>
2022-03-07 23:12:19 +0900

    eeee

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,58 @@
 
 <template>
-
+<!-- 
 <div class = "black-bg" v-if="모달창열렸니 == true">
   <div class = "white-bg">
   <h4>{{원룸들[누른거].title}}</h4>
   <p>상세페이지 내용임</p>
   <button @click = "모달창열렸니 = false">닫기</button>
   </div>
-</div>
+</div> -->
 
 
 <div class = "menu">
   <a v-for= "a in 메뉴들" :key="a">{{a}}</a>
 </div>
 
-
+<!-- 
 <div v-for= "(a,i) in 원룸들" :key="i">
   <img :src= "원룸들[누른거].image" class = "room-img">
 
   <h4 @click = "모달창열렸니 = true; 누른거 = i">{{원룸들[i].title}}</h4>
   <p>{{원룸들[i].price}}원</p>
-</div>
+</div> -->
 
-
+<Card :원룸="원룸들[0]"/>
+<Card :원룸="원룸들[1]"/>
+<Card :원룸="원룸들[2]"/>
+<Card :원룸="원룸들[3]"/>
+<Card :원룸="원룸들[4]"/>
+<Card :원룸="원룸들[5]"/>
 
 
 
 </template>
 
 <script>
-import data from './oneroom.js'
-import modal from './Modal.vue'
+import data from './oneroom.js' ;
+// import Modal from './Modal.vue' ;
+import Card from './Card.vue' ;
 
 export default {
   name: 'App',
   data(){
     return {
-      모달창열렸니 : modal,
+      // 모달창열렸니 : Modal,
       누른거 : 0,
       원룸들 : data,
       메뉴들 : ['home', 'shops', 'about'],
       products : ['역삼동원룸', '천호동원롬', '마포구원룸']
     }
+  },
+  components: {
+    // Discount : Discount,
+    // Modal : Modal,
+    Card : Card,
   }
 }
 
@@ -53,7 +64,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #1e2c3a;
   margin-top: 60px;
 }
 
--- src/card.vue
+++ src/card.vue
@@ -1,35 +1,18 @@
 <template>
-  <div v-for ="(a,i) in 원룸들" :key="i">
-      <img :src="a.image" class="room-img">
-      <h4 @click="모달창열렸니 = true; 누른거 = i">{{a.title}}</h4>
-      <p>{{a.price}}원</p>
+  <div>
+      <img :src="원룸.image" class="room-img">
+      <h4>{{원룸.title}}</h4>
+      <!-- <h4 @click="모달창열렸니 = true; 누른거 = i">{{a.title}}</h4> -->
+      <p>{{원룸.price}}원</p>
   </div>
 </template>
 
-<script>
-export default {
-    name : 'card'
-
-}
-</script>
-
-<style>
-
-</style>
-
-<template>
-  <div>
-    <img :src="원룸.image" class="room-img">
-    <h4> {{원룸.title}} </h4>
-    <p>{{원룸.price}} 원</p>
-   </div>
-</template>
 
 
 <script>
 export default {
   props : {
-    원룸 : Object
+    원룸 : Object,
   }
 }
 </script>
